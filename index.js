7690678d28e2a9efff2d95005059c273a5988c1f
Carlos Eduardo <65124135+KingTeox@users.noreply.github.com>
2021-12-10 16:27:15 -0300

    <M><A><I><N>

--- index.js
+++ index.js
@@ -31,8 +31,416 @@ LogsExtra(Client, {
 
 
 
+Client.on("guildChannelTopicUpdate", async (channel, oldT, newT) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let ctUP = new Discord.MessageEmbed()
+    ctUP.setTitle("ChannelTopicUpdate")
+    ctUP.setColor("PURPLE")
+    ctUP.addField(`Channel:`, `${channel.name}[${channel.id}]`)
+    ctUP.addField(`Topic:`, `${oldT} => ${newT}`)
+    ctUP.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${channel.guild.iconURL()}` != "null") {
+        ctUP.setThumbnail(`${channel.guild.iconURL({dynamic: true})}`)
+    } else {
+        ctUP.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    ctUP.setTimestamp()
+
+    Logs.send({embeds: [ctUP]})
+})  
+
+//NT
+Client.on("guildChannelPermissionsUpdate", async (channel, oldPerm, newPerm) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    //channel name emit this
+    let cpUP = new Discord.MessageEmbed()
+    cpUP.setTitle("ChannelUpdate")
+    cpUP.setColor("PURPLE")
+    cpUP.addField(`Channel:`, `${channel.name}[${channel.id}]`)
+    cpUP.addField(`Not`, `Not`)
+    cpUP.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${channel.guild.iconURL()}` != "null") {
+        cpUP.setThumbnail(`${channel.guild.iconURL({dynamic: true})}`)
+    } else {
+        cpUP.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    cpUP.setTimestamp()
+
+    Logs.send({embeds: [cpUP]})
+})
+
+//um
+Client.on("guildMemberBoost", async (member) => {
+    
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let meBO = new Discord.MessageEmbed()
+    meBO.setTitle(`${member.user.tag} Give Boost Thanks <3`)
+    meBO.setColor("PURPLE")
+    meBO.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    meBO.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        meBO.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        meBO.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    meBO.setTimestamp()
 
 
+    Logs.send({embeds: [meBO]})
+})
+
+//um
+Client.on("guildMemberUnboost", async (member) => {
+    
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let meUNBO = new Discord.MessageEmbed()
+    meUNBO.setTitle(`${member.user.tag} Remove Boost`)
+    meUNBO.setColor("PURPLE")
+    meUNBO.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    meUNBO.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        meUNBO.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        meUNBO.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    meUNBO.setTimestamp()
+
+
+    Logs.send({embeds: [meUNBO]})
+})
+
+Client.on("guildMemberRoleAdd", async (member, role) => {
+    
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let roAD = new Discord.MessageEmbed()
+    roAD.setTitle("RoleAdd")
+    roAD.setColor("PURPLE")
+    roAD.addField(`Role:`, `${role.name}[${role.id}]`)
+    roAD.addField(`User:`, `${member.user.tag}[${member.user.id}]`)
+    roAD.addField(`UserRoles:`, `${member.roles.cache.map(role => role.toString()).join(' ')}`)
+    roAD.addField(`RolePerms:`, `\`\`\`bash\n"${role.permissions.toArray().toString().replace(/,/gi, `",\n"`)}\n\`\`\``)
+    roAD.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        roAD.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        roAD.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    roAD.setTimestamp()
+
+
+    Logs.send({embeds: [roAD]})
+})
+
+Client.on("guildMemberRoleRemove", async (member, role) => {
+    
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let roRM = new Discord.MessageEmbed()
+    roRM.setTitle("RoleRemove")
+    roRM.setColor("PURPLE")
+    roRM.addField(`Role:`, `${role.name}[${role.id}]`)
+    roRM.addField(`User:`, `${member.user.tag}[${member.user.id}]`)
+    roRM.addField(`UserRoles:`, `${member.roles.cache.map(role => role.toString()).join(' ')}`)
+    roRM.addField(`RolePerms:`, `\`\`\`bash\n"${role.permissions.toArray().toString().replace(/,/gi, `",\n"`)}\n\`\`\``)
+    roRM.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        roRM.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        roRM.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    roRM.setTimestamp()
+
+
+
+    Logs.send({embeds: [roRM]})
+})
+
+Client.on("guildMemberNicknameUpdate", async (member, oldN, newN) => {
+    
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let niUP = new Discord.MessageEmbed()
+    niUP.setTitle("MemberNicknameUpdate")
+    niUP.setColor("PURPLE")
+    niUP.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    niUP.addField(`NickName:`, `${oldN} => ${newN}`)
+    niUP.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        niUP.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        niUP.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    niUP.setTimestamp()
+
+
+
+    Logs.send({embeds: [niUP]})
+})
+
+Client.on("messagePinned", async (message) => {
+    
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+    await message.fetch().then(json => { 
+        
+        let mePI = new Discord.MessageEmbed()
+        mePI.setTitle("MessagePined")
+        mePI.setColor("PURPLE")
+        mePI.addField(`Channel:`, `${message.channel.name}[${message.channel.id}]`)
+        if (message.content != "" || message.content != "null") {
+            if (message.content.leght >= 1000) {
+                mePI.setDescription(`MessageContent:\`\`\`\n${message.content}\n\`\`\``)
+            } else {
+                mePI.addField(`MessageContent:`, `\`\`\`\n${message.content}\n\`\`\``)
+            }
+        } 
+        mePI.addField(`MessageAuthor:`, `${message.author.tag}[${message.author.id}]`)
+        mePI.addField(`MessageType:`, `${message.type}`)
+        mePI.addField(`MessageURL:`, `**[here](${message.url})**`)
+        mePI.addField(`React MessageFetchJson:`, `<:json_:898299786657140746>`)
+        mePI.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+        if (`${message.guild.iconURL()}` != "null") {
+            mePI.setThumbnail(`${message.guild.iconURL({dynamic: true})}`)
+        } else {
+            mePI.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+        }
+        mePI.setTimestamp()
+
+        
+
+        Logs.send({embeds: [mePI]}).then(msg => {
+            
+            msg.react("898299786657140746").then(r2 => {})
+        
+            const filteraa = (reaction, user) => !user.bot && reaction.emoji.name === `json_` && reaction.users.remove(user) ;
+            const COLE = msg.createReactionCollector({filter: filteraa})
+    
+        
+            let JSONEMB = new Discord.MessageEmbed()
+            JSONEMB.setTitle("JsonMessageFetch")
+            JSONEMB.setColor("PURPLE")
+            JSONEMB.setDescription(`Json:\`\`\`js\n${JSON.stringify(json, null, 3)}\`\`\``)
+
+            COLE.on("collect", async (reac, user) => {
+                msg.reply({content: `${user} Your MessageFetchJson`, embeds: [JSONEMB]})
+                COLE.emit("end", "a")
+            })
+        })
+    })
+})
+
+Client.on("voiceChannelJoin", async (member, channel) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let vcJN = new Discord.MessageEmbed()
+    vcJN.setTitle("VoiceChannelJoin")
+    vcJN.setColor("PURPLE")
+    vcJN.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    vcJN.addField(`VoiceChannel:`, `${channel}[${channel.id}]`)
+    if (channel.members === "undefined" || !channel.members) {
+        vcJN.addField(`MembersList:`, `**Undefined**`)
+    } else {
+        vcJN.addField(`MembersList:`, `**${channel.members.map(mem => `${mem.user.tag}[${mem.user.id}]\n`).join(` `)}**`)  
+    }
+    vcJN.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        vcJN.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        vcJN.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    vcJN.setTimestamp()
+
+
+    Logs.send({embeds: [vcJN]})
+})
+
+Client.on("voiceChannelLeave", async (member, channel) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let vcLV = new Discord.MessageEmbed()
+    vcLV.setTitle("VoiceChannelLeave")
+    vcLV.setColor("PURPLE")
+    vcLV.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    vcLV.addField(`VoiceChannel:`, `${channel}[${channel.id}]`)
+    if (channel.members === "undefined" || !channel.members) {
+        vcLV.addField(`MembersList:`, `**Undefined**`)
+    } else {
+        vcLV.addField(`MembersList:`, `**${channel.members.map(mem => `${mem.user.tag}[${mem.user.id}]\n`).join(` `)}**`)  
+    }
+    vcLV.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        vcLV.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        vcLV.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    vcLV.setTimestamp()
+
+
+    Logs.send({embeds: [vcLV]})
+})
+
+Client.on("voiceChannelSwitch", async (member, oldC, newC) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let vcSW = new Discord.MessageEmbed()
+    vcSW.setTitle("VoiceChannelSwitch")
+    vcSW.setColor("PURPLE")
+    vcSW.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    vcSW.addField(`ChannelSwitch:`, `${oldC} => ${newC}`)
+    if (oldC.members === "" || oldC.members.map(mi => `${mi.user.tag}`).join(` `) === "") {
+        vcSW.addField(`oldChannelMembersList:`, `**Undefined or No have Members**`)
+    } else {
+        vcSW.addField(`oldChannelMembersList:`, `**${oldC.members.map(memb => `${memb.user.tag}[${memb.user.id}]\n`).join(` `)}**`)
+    }
+    vcSW.addField(`newChannelMembersList:`, `**${newC.members.map(memb => `${memb.user.tag}[${memb.user.id}]\n`).join(` `)}**`)
+    vcSW.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        vcSW.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        vcSW.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    vcSW.setTimestamp()
+
+
+    Logs.send({embeds: [vcSW]})
+})
+
+Client.on("voiceChannelMute", async (member, type) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let vcMT = new Discord.MessageEmbed()
+    vcMT.setTitle("MemberMute")
+    vcMT.setColor("PURPLE")
+    vcMT.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    vcMT.addField(`MuteType:`, `**${type.toUpperCase().replace("-", " ")}**`)
+    vcMT.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        vcMT.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        vcMT.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    vcMT.setTimestamp()
+
+
+    Logs.send({embeds: [vcMT]})
+})
+
+Client.on("voiceChannelUnmute", async (member, type) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let vcUMT = new Discord.MessageEmbed()
+    vcUMT.setTitle("MemberUnmute")
+    vcUMT.setColor("PURPLE")
+    vcUMT.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    vcUMT.addField(`UnmuteType:`, `**${type.toUpperCase().replace("-", " ")}**`)
+    vcUMT.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        vcUMT.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        vcUMT.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    vcUMT.setTimestamp()
+
+
+    Logs.send({embeds: [vcUMT]})
+})
+
+Client.on("voiceChannelDeaf", async (member, type) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let vcDF = new Discord.MessageEmbed()
+    vcDF.setTitle("MemberDeaf")
+    vcDF.setColor("PURPLE")
+    vcDF.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    vcDF.addField(`DeafType:`, `**${type.toUpperCase().replace("-", " ")}**`)
+    vcDF.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        vcDF.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        vcDF.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    vcDF.setTimestamp()
+
+
+    Logs.send({embeds: [vcDF]})
+})
+
+Client.on("voiceChannelUndeaf", async (member, type) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let vcUDF = new Discord.MessageEmbed()
+    vcUDF.setTitle("MemberUndeaf")
+    vcUDF.setColor("PURPLE")
+    vcUDF.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    vcUDF.addField(`UndeafType:`, `**${type.toUpperCase().replace("-", " ")}**`)
+    vcUDF.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        vcUDF.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        vcUDF.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    vcUDF.setTimestamp()
+
+
+    Logs.send({embeds: [vcUDF]})
+})
+
+Client.on("voiceStreamingStart", async (member, channel) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let vcSTST = new Discord.MessageEmbed()
+    vcSTST.setTitle("MemberStartStream")
+    vcSTST.setColor("PURPLE")
+    vcSTST.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    vcSTST.addField(`Channel:`, `${channel}[${channel.id}]`)
+    vcSTST.addField(`ChannelMembersList:`, `**${channel.members.map(mem => `${mem.user.tag}[${mem.user.id}]\n`).join(` `)}**`)  
+    vcSTST.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        vcSTST.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        vcSTST.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    vcSTST.setTimestamp()
+
+
+    Logs.send({embeds: [vcSTST]})
+})
+
+Client.on("voiceStreamingStop", async (member, channel) => {
+
+    let Logs = Client.channels.cache.get(ids.Channels.Logs)
+
+    let vcSTSP = new Discord.MessageEmbed()
+    vcSTSP.setTitle("MemberStopStream")
+    vcSTSP.setColor("PURPLE")
+    vcSTSP.addField(`Member:`, `${member.user.tag}[${member.user.id}]`)
+    vcSTSP.addField(`Channel:`, `${channel}[${channel.id}]`)
+    vcSTSP.addField(`ChannelMembersList:`, `**${channel.members.map(mem => `${mem.user.tag}[${mem.user.id}]\n`).join(` `)}**`)  
+    vcSTSP.setFooter(`Logs By Teox <3 | POISONED-DEVS.DE on Top`)
+    if (`${member.guild.iconURL()}` != "null") {
+        vcSTSP.setThumbnail(`${member.guild.iconURL({dynamic: true})}`)
+    } else {
+        vcSTSP.setThumbnail(`${Client.user.avatarURL({dynamic: true})}`)
+    }
+    vcSTSP.setTimestamp()
+
+
+    Logs.send({embeds: [vcSTSP]})
+})
 
 
 
@@ -46,7 +454,7 @@ LogsExtra(Client, {
 
 Client.on("messageCreate", async (message) => {
 
-//code
+//CODE 
     
 })
 
